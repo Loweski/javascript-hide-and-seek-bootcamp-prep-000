eb2598c6b492478f3b69b9a945ce04c47ab1a01a
Leng Vang <lengvang9000@gmail.com>
2017-08-25 03:25:08 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  /*var list = document.querySelectorAll('div#grand-node')
+  var list = document.querySelectorAll('div#grand-node')
 
   var children = []
   var counter = 0;
@@ -34,14 +34,5 @@ function deepestChild(){
       deepest = currChild
     }
   }
-  return deepest*/
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
-  }
-
-  return node
+  return deepest
 }
